85fa38a10387a3ac2214fdb4fe227a9e0b7ca17f
mxylovezh <18322276773@163.com>
2020-06-05 01:19:50 +0800

    完成用户登录功能

--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -1,13 +1,40 @@
 // miniprogram/pages/user/user.js
+const db = wx.cloud.database()
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    isLogin: false,
+    disabled: true
   },
-
+  bindGetUserInfo(event){
+    const userInfo = event.detail.userInfo
+    if(!this.data.isLogin && userInfo){
+      db.collection('users').add({
+        data: {
+          avatarUrl: userInfo.avatarUrl,
+          nickName: userInfo.nickName,
+          signature: '',
+          phone: '',
+          favor: 0,
+          wxNumber: '',
+          createAt: new Date()
+        }
+      }).then(result=>{
+        db.collection('users').doc(result._id).get().then(result=>{
+          app.userInfo = JSON.parse(JSON.stringify(result.data)) 
+          this.setData({
+            userInfo: app.userInfo,
+            isLogin: true
+          })
+        })
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -19,7 +46,26 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    wx.cloud.callFunction({
+      name: 'login',
+      data:{}
+    }).then(result=>{
+      db.collection('users').where({
+        _openid: result.result.openid
+      }).get().then(result=>{
+        if(result.data.length === 0){
+          this.setData({
+            disabled: false
+          })
+          return
+        }
+        app.userInfo = JSON.parse(JSON.stringify(result.data[0]))
+        this.setData({
+          userInfo: app.userInfo,
+          isLogin: true
+        })
+      })
+    })
   },
 
   /**
